3f3613361c24bc57a2e09dc294f742ba3e93d172
Hanqing Zhao <hanqingzh@tju.edu.cn>
2015-12-27 17:04:30 -0500

    New project
    Share the idea of using pointer
    loop and swap two objects

---
+++ src/Array/SortColors.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SortColors {
+	public static void main(String args[]){
+		int aNums[] = {1,1,2,2,0,2,0};
+		sortColors(aNums);
+	}
+	public static void sortColors(int[] nums) {
+        int flag=0;
+        int temp;
+        for(int color =0; color<3; color++){
+            for(int i=0; i< nums.length; i++){
+                if(nums[i]==color){
+                	//swap nums[i] and nums[flag]
+                    temp=nums[flag];
+                    nums[flag]=nums[i];
+                    nums[i]=temp;
+                    
+                    flag++;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
