7aa5bc9cb420f5423260f4363a0f606bc98ee7dd
Hanqing Zhao <hanqingzh@tju.edu.cn>
2015-12-27 10:10:40 -0500

    New project

---
+++ src/Array/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RemoveDuplicatesfromSortedArray {
+	public static void main(String args[]){
+		int aNums[] = {1,1,2,2,3,3,4};
+		System.out.println(removeDuplicates(aNums));
+	}
+	public static int removeDuplicates(int[] nums){ 
+        int flag = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[flag]=nums[i];
+                flag++;
+            }
+        }
+        return flag;
+    }
+}
--- src/Math/nimGame.java
+++ src/Math/nimGame.java
@@ -3,8 +3,9 @@ package Math;
 public class nimGame {
 	public static void main(String args[]){
 		System.out.println(canWinNim(4));
+		//Input number
 	}
 	public static boolean canWinNim(int n){
-	        return ( n % 4 != 0);
+	     return ( n % 4 != 0);
 	}
 }
