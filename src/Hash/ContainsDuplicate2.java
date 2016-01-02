4ddd98c6d42ea389410b45db23897d3a29fe8639
Hanqing Zhao <hanqingzh@tju.edu.cn>
2016-01-02 16:05:52 -0500

    New method
    hashset remove

--- src/Hash/ContainsDuplicate2.java
+++ src/Hash/ContainsDuplicate2.java
@@ -9,25 +9,33 @@ public class ContainsDuplicate2 {
 		System.out.println(containsNearbyDuplicate(numbers,2));
 		//Input number
 	}
+//	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+//        Set<Integer> dSet = new HashSet<Integer>();
+//        for(int i=0; i<nums.length; i++){
+//            if(!dSet.add(nums[i])){
+//                if(i-k>0){
+//                for(int j=i-k; j<i; j++){
+//                    if(nums[j]==nums[i])
+//                        return true;
+//                }
+//                }
+//                else{
+//                for(int j=0; j<i; j++){
+//                    if(nums[j]==nums[i])
+//                        return true;
+//                }   
+//                }
+//            }
+//          }
+//        return false;
+//    }
 	public static boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> dSet = new HashSet<Integer>();
         for(int i=0; i<nums.length; i++){
-            if(!dSet.add(nums[i])){
-                if(i-k>0){
-                for(int j=i-k; j<i; j++){
-                    if(nums[j]==nums[i])
-                        return true;
-                }
-                }
-                else{
-                for(int j=0; j<i; j++){
-                    if(nums[j]==nums[i])
-                        return true;
-                }   
-                }
-            }
-            
-        }
-        return false;
+            if(i>k) dSet.remove(nums[i-k-1]);
+        	if (!dSet.add(nums[i]))
+	        	return true;
+	        }
+	        return false;
     }
 }
