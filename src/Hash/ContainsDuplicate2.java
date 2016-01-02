a90425993c0f6ff1393331936d8dcaf608222f9c
Hanqing Zhao <hanqingzh@tju.edu.cn>
2016-01-02 15:59:46 -0500

    New project
    Hashset

---
+++ src/Hash/ContainsDuplicate2.java
@@ -0,0 +1,33 @@
+package Hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate2 {
+	public static void main(String args[]){
+		int[] numbers = new int[]{1,2,2,3};
+		System.out.println(containsNearbyDuplicate(numbers,2));
+		//Input number
+	}
+	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> dSet = new HashSet<Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(!dSet.add(nums[i])){
+                if(i-k>0){
+                for(int j=i-k; j<i; j++){
+                    if(nums[j]==nums[i])
+                        return true;
+                }
+                }
+                else{
+                for(int j=0; j<i; j++){
+                    if(nums[j]==nums[i])
+                        return true;
+                }   
+                }
+            }
+            
+        }
+        return false;
+    }
+}
