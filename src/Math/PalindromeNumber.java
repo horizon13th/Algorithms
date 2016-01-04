3471dbbb351c8d3497326a48b0c7be514b70ee96
horizon13th <hanqingzh@tju.edu.cn>
2016-01-04 15:27:08 -0500

    New project
    uses the idea of scroll from two ends and compare each pair of numbers

---
+++ src/Math/PalindromeNumber.java
@@ -0,0 +1,27 @@
+package Math;
+
+public class PalindromeNumber {
+	public static void main(String args[]){
+		int number = 121;
+		System.out.println(isPalindrome(number));
+	}
+	public static boolean isPalindrome(int x) {
+        if(x>=0 && x<Integer.MAX_VALUE){
+        String num = new String(x+"");
+        
+        int begin = 0;
+        int end = num.length()-1;
+        
+        while(begin<end){
+            if(num.charAt(begin)!=num.charAt(end)){
+                return false;
+            }
+            begin++;
+            end--;
+        }
+        }
+        else
+            return false;
+        return true;
+    }
+}
