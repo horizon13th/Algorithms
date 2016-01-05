ba1040e75906cf4898c39f7742f67bfac964c59b
horizon13th <hanqingzh@tju.edu.cn>
2016-01-04 19:03:37 -0500

    New project

--- Notes.md
+++ Notes.md
@@ -2,13 +2,16 @@ Notes
 =================
 
 ##Array#
+int[] arr = new int[2];
+String[] arr = new String[]{"11","22"}
 
 ##Math#
 
 ##String#
-
-String xx = new String()  
-String.length()   
+String xx = new String() //declare and allocate
+String.length() 
+String.substring(int begin, int end)  
+String.indexOf(String str)//can be used to check prefix
 
 StringBuffer  
 StringBuffer sb = new StingBuffer()  
---
+++ src/String/LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package String;
+
+public class LongestCommonPrefix {
+	public static void main(String args[]){
+		String[] number = new String[]{ "121","12"};
+		System.out.println(longestCommonPrefix(number));
+	}
+	public static String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)
+            return "";
+        String pre = strs[0];
+        for(int i=0;i<strs.length;i++){
+            while(strs[i].indexOf(pre)!=0){//check if pre exists
+                pre=pre.substring(0,pre.length()-1);//substring 1 digit from right
+            }
+        }
+        return pre;
+    }
+}
