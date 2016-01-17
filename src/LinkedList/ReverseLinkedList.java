069a43034ac3b7c1551085112604fc7588261e94
horizon13th <hanqingzh@tju.edu.cn>
2016-01-17 17:00:45 -0500

    New method

--- src/LinkedList/ReverseLinkedList.java
+++ src/LinkedList/ReverseLinkedList.java
@@ -18,4 +18,25 @@ public class ReverseLinkedList {
         
         return p1;
     }
+	public static ListNode reverseListR(ListNode head){
+		if(head==null||head.next==null)
+			return head;
+		
+		ListNode root =reverseListR(head.next);
+		head.next.next = head;
+		head.next = null;
+		return root;
+		//very difficult to understand
+		//1-->2-->3-->4
+		//use recursive to return the last node 4 head=3
+		//then the last second node's next will change it's next to last second node 3<--4
+		//let the last second node pointing to null null<--3<--4
+		//return the root(the last node) 4 head=2
+		//the last third node's next will change it's next to last third node 2<--3
+		//let the last third node pointing to null null<--2<--3<--4
+		//return the root(the last node) 4 head=1
+		//1<--2
+		//null<--1<--2<--3<--4
+		//return root=4
+	}
 }
