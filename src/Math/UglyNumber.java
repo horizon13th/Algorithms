f88f918649bcd6093c62eb9f42d9aa7c0b2e7a30
horizon13th <hanqingzh@tju.edu.cn>
2016-01-21 16:09:02 -0500

    New Project

---
+++ src/Math/UglyNumber.java
@@ -0,0 +1,21 @@
+package Math;
+
+public class UglyNumber {
+	public static void main(String args[]){
+		int number = 30;
+		//89, 1, 0
+		System.out.println(isUgly(number));
+	}
+    public static boolean isUgly(int num) {  	
+    	num = modnum(num,2);
+    	num = modnum(num,3);
+    	num = modnum(num,5);
+    	return (num==1);       	
+    }
+    public static int modnum(int num, int div){
+    	while(num%div==0){
+    		num = num/div;
+    	}
+    	return num;
+    }
+}
