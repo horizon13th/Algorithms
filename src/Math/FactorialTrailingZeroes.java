303a87ed0b8f8c4e1d132dc27ca9b7d907f87920
horizon13th <hanqingzh@tju.edu.cn>
2016-01-21 21:16:24 -0500

    New Project

---
+++ src/Math/FactorialTrailingZeroes.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class FactorialTrailingZeroes {
+	public static void main(String args[]){
+		int num = 10;//26
+		System.out.println(trailingZeroes(num));
+	}
+	public static int trailingZeroes(int n) {
+        return n==0? 0: n/5+trailingZeroes(n/5);
+    }
+}
