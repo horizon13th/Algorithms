1e74039a7a204e62cd8572738f8b3576227d9f07
horizon13th <hanqingzh@tju.edu.cn>
2016-01-23 17:03:47 -0500

    New project

---
+++ src/Stack/ImplementQueueusingStacks.java
@@ -0,0 +1,40 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ImplementQueueusingStacks {
+	private Stack<Integer> input =new Stack<Integer>();
+	private Stack<Integer> output =new Stack<Integer>();
+	// Push element x to the back of queue.
+    public void push(int x) {
+        input.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        while(!input.isEmpty()){
+        	output.push(input.pop());
+        }
+        output.pop();
+        while(!output.isEmpty()){
+        	input.push(output.pop());
+        }
+    }
+
+    // Get the front element.
+    public int peek() {
+    	while(!input.isEmpty()){
+        	output.push(input.pop());
+        }
+        int result=output.peek();
+        while(!output.isEmpty()){
+        	input.push(output.pop());
+        }
+        return result;
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return input.isEmpty();
+    }
+}
