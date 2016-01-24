d3098194036ec073516093e126ae77acae4c372c
horizon13th <hanqingzh@tju.edu.cn>
2016-01-23 23:34:03 -0500

    New project

--- src/Stack/ImplementStackusingQueues.java
+++ src/Stack/ImplementStackusingQueues.java
@@ -1,5 +1,34 @@
 package Stack;
 
-public class ImplementStackusingQueues {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+public class ImplementStackusingQueues {
+	private Queue<Integer> q = new LinkedList<Integer>();
+	// Push element x onto stack.
+    public void push(int x) {
+        q.add(x);
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        for(int i=0;i<q.size()-1;i++)
+        	q.add(q.remove());
+        q.remove();
+    }
+
+    // Get the top element.
+    public int top() {
+    	for(int i=0;i<q.size()-1;i++)
+        	q.add(q.remove());
+    	int result=q.remove();
+    	q.add(result);
+    	return result;
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q.isEmpty();
+    }
 }
---
+++ src/Stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+	public static void main(String args[]){
+		String str1 = "]";
+		System.out.println(isValid(str1));
+	}
+	 public static boolean isValid(String s) {
+	       Stack<Character> st=new Stack<Character>();
+	       for(int i=0;i<s.length();i++){
+	    	   	char ch = s.charAt(i);
+	            if(ch=='('||ch=='{'||ch=='[')
+	                st.push(ch);
+	            else if(ch==')'&&!st.empty()&&st.peek()=='(')
+	                st.pop();
+	            else if(ch==']'&&!st.empty()&&st.peek()=='[')
+	                st.pop();
+	            else if(ch=='}'&&!st.empty()&&st.peek()=='{')
+	                st.pop();
+	            else
+	            	return false;
+	       }
+	       return st.empty();
+	    }
+}
