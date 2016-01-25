96466fe3ba4b0168afac7b219b6109ed3ea6bd09
horizon13th <hanqingzh@tju.edu.cn>
2016-01-24 22:05:26 -0500

    New project

---
+++ src/Tree/BinaryTreePaths.java
@@ -0,0 +1,26 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+	public static void main(String[] args){
+		TreeNode r=new TreeNode(0);
+		r.left=new TreeNode(1);
+		r.right=new TreeNode(2);
+		r.left.left=new TreeNode(2);
+		r.left.left.left=new TreeNode(10);
+		System.out.println(binaryTreePaths(r));
+	}
+	public static List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        if(root!=null)	search(root,"",result);
+        return result;
+    }
+	public static void search(TreeNode root, String path, List<String> result){
+		if(root.left==null&&root.right==null)	result.add(path+root.val);
+		if(root.left!=null)		search(root.left,path+root.val+"->",result);
+		if(root.right!=null)	search(root.right,path+root.val+"->",result);
+	}
+	
+}
