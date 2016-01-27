ed199046e7bf551e315448c1335d53e018af9161
horizon13th <hanqingzh@tju.edu.cn>
2016-01-27 09:14:06 -0500

    New method

--- src/LinkedList/LinkedListCycle.java
+++ src/LinkedList/LinkedListCycle.java
@@ -12,4 +12,21 @@ public class LinkedListCycle {
         }
         return false;
     }
+	public boolean hasCycle1(ListNode head) {
+		//according to pysical theory, 
+		//we can see one node as stop node, 
+		//another node as running node. 
+		//So the relavant spped is 1 node/time.
+        if(head==null)
+            return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
 }
