796a714d3f8e53f89d6be5764574c6ebf18a9ecd
horizon13th <hanqingzh@tju.edu.cn>
2016-01-26 21:48:31 -0500

    New method

--- src/LinkedList/ReverseLinkedList.java
+++ src/LinkedList/ReverseLinkedList.java
@@ -2,21 +2,18 @@ package LinkedList;
 
 public class ReverseLinkedList {
 	public ListNode reverseList(ListNode head) {
-		if (head == null || head.next == null)
-			return head;
-		ListNode p1 = head;
-		ListNode p2 = head.next;
-		ListNode temp;
-
-		head.next = null;
-		while (p2 != null) {
-			temp = p2.next;
-			p2.next = p1;
-			p1 = p2;
-			p2 = temp;
-		}
-
-		return p1;
+		if(head==null||head.next==null)
+            return head;
+        ListNode tempHead=head;
+        ListNode p1=head;
+        ListNode p2=head.next;
+        while(p2!=null){
+            p1.next=p2.next;
+            p2.next=tempHead;
+            tempHead=p2;
+            p2=p1.next;
+        }
+        return tempHead;
 	}
 
 	public static ListNode reverseListR(ListNode head) {
