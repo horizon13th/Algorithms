562f42b30d34231b18335fcf7186fb243e2af44b
horizon13th <hanqingzh@tju.edu.cn>
2016-01-30 16:19:56 -0500

    New project

---
+++ src/Matrix/SpiralMatrix2.java
@@ -0,0 +1,31 @@
+package Matrix;
+
+import java.util.Arrays;
+
+public class SpiralMatrix2 {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(generateMatrix(3)));
+	}
+
+	public static int[][] generateMatrix(int n) {
+		int[][] result = new int[n][n];
+		if (n == 0)
+			return result;
+		int level = n / 2;
+		int flag = n % 2;
+		int num = 1;
+		for (int i = 0; i < level; i++) {
+			for (int j = i; j < n - 1 - i; j++)
+				result[i][j] = num++;
+			for (int j = i; j < n - 1 - i; j++)
+				result[j][n - i - 1] = num++;
+			for (int j = n - i - 1; j > i; j--)
+				result[n - 1 - i][j] = num++;
+			for (int j = n - i - 1; j > i; j--)
+				result[j][i] = num++;
+		}
+		if (flag == 1)
+			result[level][level] = num++;
+		return result;
+	}
+}
