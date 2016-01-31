b1081b3d5a14e06f05f6b76c0490cbdb5a5863db
horizon13th <hanqingzh@tju.edu.cn>
2016-01-30 22:47:49 -0500

    New project

--- src/Tree/PathSum/BinaryTreeMaximumPathSum.java
+++ src/Tree/PathSum/BinaryTreeMaximumPathSum.java
@@ -1,5 +1,29 @@
 package Tree.PathSum;
 
-public class BinaryTreeMaximumPathSum {
+import Tree.TreeNode;
 
+public class BinaryTreeMaximumPathSum {
+	public static void main(String[] args) {
+		TreeNode r = new TreeNode(1);
+		r.left = new TreeNode(-2);                                                                       
+		r.left.left = new TreeNode(1);
+		r.left.right = new TreeNode(3);
+		r.left.left.left = new TreeNode(-1);
+		r.right = new TreeNode(-3);   
+		r.right.left = new TreeNode(-2);   
+		System.out.println(maxPathSum(r));
+	}
+	public static int max;
+	public static int maxPathSum(TreeNode root) {
+		max=Integer.MIN_VALUE;
+		helper(root);
+		return max;
+	}
+	public static int helper(TreeNode t){
+		if(t==null)	return 0;
+		int left=Math.max(0, helper(t.left));
+		int right=Math.max(0, helper(t.right));
+		max=Math.max(max, left+right+t.val);
+		return Math.max(left, right)+t.val;
+	}
 }
