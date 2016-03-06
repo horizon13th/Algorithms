e47ed72fcdfee49561f81616cd65a9d54374f318
horizon13th <hanqingzh@tju.edu.cn>
2016-03-06 14:27:45 -0500

    New project

---
+++ src/Backtracking/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+	public void main(String []args){
+		System.out.println(generateParenthesis(5));
+	}
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if(n==0)
+            return res;
+        String str = "";
+        helper(res, str, 0, 0, n);
+        return res;
+    }
+    public void helper(List<String>res, String str, int left, int right, int n){
+        if(right>left)
+            return;
+        if(left==n && right==n){
+            res.add(str);
+            return;
+        }
+        if(left<n){
+            helper(res, str+"(", left+1, right, n);
+        }
+        if(right<n){
+            helper(res, str+")", left, right+1, n);
+        }
+    }
+}
