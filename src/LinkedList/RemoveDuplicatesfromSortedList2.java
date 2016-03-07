fb1f047955776be5b1540a2af5b408e6b0b75e84
horizon13th <hanqingzh@tju.edu.cn>
2016-03-07 13:18:25 -0500

    New project

--- src/LinkedList/RemoveDuplicatesfromSortedList2.java
+++ src/LinkedList/RemoveDuplicatesfromSortedList2.java
@@ -1,5 +1,26 @@
 package LinkedList;
 
 public class RemoveDuplicatesfromSortedList2 {
+	public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while(head!=null && head.next!=null){
+            if(head.val==head.next.val){
+                int dup = head.val;
+                while(head!=null && head.val==dup){
+                    head=head.next;
+                }
+                pre.next=head;
+            }
+            else{
+                head=head.next;
+                pre=pre.next;
+            }
+        }
 
+        return dummy.next;
+    }
 }
