7a2984c7fc07a060fa388f4c723e67df6ec0cdc8
horizon13th <hanqingzh@tju.edu.cn>
2016-03-12 21:03:21 -0500

    New method

--- src/String/LongestPalindromicSubstring.java
+++ src/String/LongestPalindromicSubstring.java
@@ -3,26 +3,31 @@ package String;
 public class LongestPalindromicSubstring {
 	public static void main(String args[]) {
 		String str = new String();
+		str = "11223322";
 		System.out.println(longestPalindrome(str));
 	}
-	public static String longestPalindrome(String s) {
-        int count=0;
-        String res=new String();
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-                String sub = s.substring(i,j);
-                //System.out.println(sub);
-                if(isPalidrome(sub)&&sub.length()>count){
-                    count=sub.length();
-                    res=sub;
-                    //System.out.println(res);
-                }
-            }
+	private static int count=1;
+    private static int begin=0;
+    public static String longestPalindrome(String s) {
+    	if(s.length()==0||s.length()==1)
+    		return s;
+        for(int i=0; i<s.length();i++){
+            find(i,0,s);
+            find(i,1,s);
+            //System.out.println(begin+""+s.substring(begin,begin+count));
         }
-        return res;
+        return s.substring(begin,begin+count);
     }
-    public static boolean isPalidrome(String s){
-        StringBuffer sb = new StringBuffer(s);
-        return sb.toString().equals(sb.reverse().toString());
+    public static void find(int i, int shift, String s){
+        int left = i;
+        int right = i + shift;
+        while(left >=0 && right <s.length() && s.charAt(left)==s.charAt(right)){
+            if(count<right-left+1){
+                begin = left;
+                count = right-left+1;
+            }
+            left--;
+            right++;
+        }
     }
 }
