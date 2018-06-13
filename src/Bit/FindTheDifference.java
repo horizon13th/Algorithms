573288c1431b2de6c6e9f0831cd75659634610ad
horizon13th <hanqingzhao1123@gmail.com>
2018-06-13 23:31:18 +0800

    #389 Add new solution

--- src/Bit/FindTheDifference.java
+++ src/Bit/FindTheDifference.java
@@ -2,6 +2,24 @@ package Bit;
 
 /**
  * Created by hanqingzhao on 6/13/18.
+ * Leetcode #389
  */
 public class FindTheDifference {
+    public static void main(String[] args){
+        String s="abbbe";
+        String t="aebbbb";
+        findTheDiff(s, t);
+    }
+    //Bitwise a^a=0 a^0=a
+    public static char findTheDiff(String s, String t) {
+        int result = 0;
+        for(int i=0; i<s.length(); i++){
+            result = result^s.charAt(i);
+        }
+        for(int j=0; j<t.length(); j++){
+            result = result^t.charAt(j);
+        }
+        //System.out.print((char)result);
+        return (char) result;
+    }
 }
